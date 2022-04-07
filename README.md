71a8af13c8224c8135bf2cd701d0c25c741f61a2
jatinsharma9871 <jatinsharma9871@gmail.com>
2022-04-07 12:38:25 +0530

    final

--- README.md
+++
@@ -1,1 +0,0 @@
-# yes-style-clone
--- yes style/src/configs/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose=require("mongoose");
-
-module.exports=()=>{
-    return mongoose.connect("mongodb+srv://yesstyle:yesstyle@cluster0.tyean.mongodb.net/YESSTYLE?retryWrites=true&w=majority");
-}
